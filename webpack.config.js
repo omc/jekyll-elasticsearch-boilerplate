64efe2b123444b122773dd02536ef82ee38a465b
Allison Zadrozny <allison@zadrozny.com>
2016-07-19 14:36:41 -0700

    Add production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // webpack folder's entry js - excluded from jekll's build process.
+  devtool: "eval",
   entry: "./webpack/entry.js",
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
---
+++ webpack.prod.config.js
@@ -0,0 +1,21 @@
+var config = require("./webpack.config.js");
+var webpack = require( "webpack");
+
+config.plugins = [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+];
+
+config.devtool = 'cheap-module-source-map';
+
+module.exports = config;
